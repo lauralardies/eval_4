eada9a66f66da4eeaa658611f8b4548a62ab45ea
lauralardies <lrodrlar@myuax.com>
2022-11-18 12:08:20 +0100

    base menú

---
+++ Evaluación 4/Ejercicios/menu.py
@@ -0,0 +1,29 @@
+def iniciar():
+
+    while True:
+
+        print('-------------------------------------')
+        print('------ Ejercicios Evaluación 4 ------')
+        print('-------------------------------------')
+        print('[1] Ejercicio 1: Comprimir mensajes')
+        print('[2] Ejercicio 2: Pokémons')
+        print('[3] Ejercicio 3: Las 7 maravillas')
+        print('[4] Salir del menú')
+
+        opcion = input('\n> ')
+
+        if opcion == '1':
+            pass
+
+        elif opcion == '2':
+            pass
+
+        elif opcion == '3':
+            pass
+
+        elif opcion == '4':
+            print('Saliendo...\n')
+            break
+
+        else:
+            print('Selecciona una opción correcta, inténtelo de nuevo\n')
