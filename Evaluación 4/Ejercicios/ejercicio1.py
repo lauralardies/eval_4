78b17a63014ed3013f98e88c8b10eaadce3f51ad
lauralardies <lrodrlar@myuax.com>
2022-11-19 13:43:45 +0100

    lista ordenada de nodos

--- Evaluación 4/Ejercicios/ejercicio1.py
+++ Evaluación 4/Ejercicios/ejercicio1.py
@@ -13,7 +13,19 @@ def insertar_nodo(raiz, dato):
     else:
         raiz.der = insertar_nodo(raiz.der, dato)
 
+def ordenar_objetos(lista):
+    lista = sorted(lista, key=lambda x: x.simbolo)
+    lista = sorted(lista, key=lambda x: x.freq)
+    return lista
+
 def arbol_huffman(simbolos, frecuencias):
     nodos = []
     for i in range(len(simbolos)): # Con este bucle tengo una lista de nodos desordenada
         nodos.append(NodoArbol(simbolos[i], frecuencias[i]))
+    nodos = ordenar_objetos(nodos)
+    return nodos
+
+simbolos = ['A', 'F', '1', '3', '0', 'M', 'T']
+frecuencias = [0.2, 0.17, 0.13, 0.21, 0.05, 0.09, 0.15]
+lista = arbol_huffman(simbolos, frecuencias)
+print('hola')
