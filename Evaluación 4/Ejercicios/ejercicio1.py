6bf657058cb0314a66c2f46f30ebb9cc60a59bed
lauralardies <lrodrlar@myuax.com>
2022-11-19 14:09:47 +0100

    avances

--- Evaluación 4/Ejercicios/ejercicio1.py
+++ Evaluación 4/Ejercicios/ejercicio1.py
@@ -18,11 +18,28 @@ def ordenar_objetos(lista):
     lista = sorted(lista, key=lambda x: x.freq)
     return lista
 
+def insertar_nodo_huffman(lista, nodo):
+    for i in range(len(lista)):
+        if lista[i].freq > nodo.freq:
+            lista.insert(i, nodo)
+            break
+        if i == len(lista):
+            lista.insert(i, nodo)
+            break
+    return lista
+
 def arbol_huffman(simbolos, frecuencias):
     nodos = []
     for i in range(len(simbolos)): # Con este bucle tengo una lista de nodos desordenada
         nodos.append(NodoArbol(simbolos[i], frecuencias[i]))
-    nodos = ordenar_objetos(nodos)
+    nodos = ordenar_objetos(nodos) # Nodos ordenados
+    while len(nodos) > 1:
+        nodo = NodoArbol('XX', nodos[0].freq + nodos[1].freq)
+        nodo.izq = nodos[0]
+        nodo.der = nodos[1]
+        nodos.pop(1)
+        nodos.pop(0)
+        nodos = insertar_nodo_huffman(nodos, nodo)
     return nodos
 
 simbolos = ['A', 'F', '1', '3', '0', 'M', 'T']
