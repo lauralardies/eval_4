0987afda5bdac58cce207a0208c38a1aaf0bc7c4
lauralardies <lrodrlar@myuax.com>
2022-11-19 14:42:36 +0100

    arreglos y comentarios

--- Evaluación 4/Ejercicios/ejercicio1.py
+++ Evaluación 4/Ejercicios/ejercicio1.py
@@ -14,17 +14,17 @@ def insertar_nodo(raiz, dato):
         raiz.der = insertar_nodo(raiz.der, dato)
 
 def ordenar_objetos(lista):
-    lista = sorted(lista, key=lambda x: x.simbolo)
-    lista = sorted(lista, key=lambda x: x.freq)
+    lista = sorted(lista, key=lambda x: x.simbolo) # Primero ordenamos alfabéticamente
+    lista = sorted(lista, key=lambda x: x.freq) # Luego ordenamos las frecuencias a partir de la lista ordenada alfabéticamente 
     return lista
 
 def insertar_nodo_huffman(lista, nodo):
     for i in range(len(lista)):
-        if lista[i].freq > nodo.freq:
+        if lista[i].freq > nodo.freq: 
             lista.insert(i, nodo)
             break
-        if i == len(lista):
-            lista.insert(i, nodo)
+        if i == len(lista)-1: # En el caso en el que tengamos que añadir nuestro nodo al final de la lista
+            lista.append(nodo)
             break
     return lista
 
@@ -34,15 +34,14 @@ def arbol_huffman(simbolos, frecuencias):
         nodos.append(NodoArbol(simbolos[i], frecuencias[i]))
     nodos = ordenar_objetos(nodos) # Nodos ordenados
     while len(nodos) > 1:
-        nodo = NodoArbol('XX', nodos[0].freq + nodos[1].freq)
+        nodo = NodoArbol('XX', nodos[0].freq + nodos[1].freq) # Creamos un nuevo nodo al que no le corresponde ningún símbolo
         nodo.izq = nodos[0]
         nodo.der = nodos[1]
+        nodos = insertar_nodo_huffman(nodos, nodo)
         nodos.pop(1)
         nodos.pop(0)
-        nodos = insertar_nodo_huffman(nodos, nodo)
     return nodos
 
 simbolos = ['A', 'F', '1', '3', '0', 'M', 'T']
 frecuencias = [0.2, 0.17, 0.13, 0.21, 0.05, 0.09, 0.15]
 lista = arbol_huffman(simbolos, frecuencias)
-print('hola')
