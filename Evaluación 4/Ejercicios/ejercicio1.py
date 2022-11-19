9fdaabfe60e91de1994e103ae081c0ae72e216ac
lauralardies <lrodrlar@myuax.com>
2022-11-19 13:27:56 +0100

    comenzamos

--- Evaluación 4/Ejercicios/ejercicio1.py
+++ Evaluación 4/Ejercicios/ejercicio1.py
@@ -1,13 +1,19 @@
 class NodoArbol:
-    def __init__(self, info):
+    def __init__(self, simbolo, freq):
         self.izq = None
         self.der = None
-        self.info = info
+        self.simbolo = simbolo
+        self.freq = freq
 
-def insertar_nodo( raiz, dato):
+def insertar_nodo(raiz, dato):
     if raiz is None:
         raiz = NodoArbol(dato)
-    elif dato < raiz.info:
+    elif dato < raiz.freq:
         raiz.izq = insertar_nodo(raiz.der, dato)
     else:
         raiz.der = insertar_nodo(raiz.der, dato)
+
+def arbol_huffman(simbolos, frecuencias):
+    nodos = []
+    for i in range(len(simbolos)): # Con este bucle tengo una lista de nodos desordenada
+        nodos.append(NodoArbol(simbolos[i], frecuencias[i]))
