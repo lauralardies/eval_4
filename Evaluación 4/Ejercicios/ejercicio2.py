25c1f2fe4c637350dfd32c85cfababa9fdbca1e9
lauralardies <lrodrlar@myuax.com>
2022-11-21 17:48:16 +0100

    arbol tipo

--- Evaluación 4/Ejercicios/ejercicio2.py
+++ Evaluación 4/Ejercicios/ejercicio2.py
@@ -27,17 +27,27 @@ def arbol_numero(raiz, nombre, num, tipo):
         raiz.izq = arbol_numero(raiz.izq, nombre, num, tipo)
     return raiz
 
+def arbol_nombre(raiz, nombre, num, tipo):
+    if raiz is None:
+        raiz = NodoArbol(nombre, num, tipo)
+    elif tipo > raiz.tipo:
+        raiz.der = arbol_numero(raiz.der, nombre, num, tipo)
+    else:
+        raiz.izq = arbol_numero(raiz.izq, nombre, num, tipo)
+    return raiz
+
 def crear_arboles(ruta):
     with open(ruta, 'r') as archivo:
         lector = reader(archivo)
-        nombres, numeros = None, None
+        nombres, numeros, tipos = None, None, None
         for i in lector:
             if i == ['#', 'Name', 'Type 1', 'Type 2', 'Total', 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Generation', 'Legendary']:
                 pass
             else:
                 nombres = arbol_nombre(nombres, i[1], i[0], i[2])
                 numeros = arbol_numero(numeros, i[1], i[0], i[2])
-    return nombres, numeros
+                tipos = arbol_nombre(tipos, i[1], i[0], i[2])
+    return nombres, numeros, tipos
 
 ruta = 'Evaluación 4/Ejercicios/pokemon.csv'
-nombres, numeros = crear_arboles(ruta)
+nombres, numeros, tipos = crear_arboles(ruta)
