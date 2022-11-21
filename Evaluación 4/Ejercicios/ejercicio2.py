772ce5bb4d3f7c14600f4b22bcc8100ca9b4e84b
lauralardies <lrodrlar@myuax.com>
2022-11-21 12:18:18 +0100

    intento de crear dos árboles de tres

--- Evaluación 4/Ejercicios/ejercicio2.py
+++ Evaluación 4/Ejercicios/ejercicio2.py
@@ -1,3 +1,5 @@
+from csv import reader
+
 class NodoArbol:
     def __init__(self, nombre, num, tipo):
         self.izq = None
@@ -6,3 +8,36 @@ class NodoArbol:
         self.nombre = nombre
         self.num = num
         self.tipo = tipo
+
+def arbol_nombre(raiz, nombre, num, tipo):
+    if raiz is None:
+        raiz = NodoArbol(nombre, num, tipo)
+    elif nombre > raiz.nombre:
+        raiz.der = arbol_numero(raiz.der, nombre, num, tipo)
+    else:
+        raiz.izq = arbol_numero(raiz.izq, nombre, num, tipo)
+    return raiz
+
+def arbol_numero(raiz, nombre, num, tipo):
+    if raiz is None:
+        raiz = NodoArbol(nombre, num, tipo)
+    elif num < raiz.num:
+        raiz.der = arbol_numero(raiz.der, nombre, num, tipo)
+    else:
+        raiz.izq = arbol_numero(raiz.izq, nombre, num, tipo)
+    return raiz
+
+def crear_arboles(ruta):
+    with open(ruta, 'r') as archivo:
+        lector = reader(archivo)
+        nombres, numeros = None, None
+        for i in lector:
+            if i == ['#', 'Name', 'Type 1', 'Type 2', 'Total', 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Generation', 'Legendary']:
+                pass
+            else:
+                nombres = arbol_nombre(nombres, i[1], i[0], i[2])
+                numeros = arbol_numero(numeros, i[1], i[0], i[2])
+    return nombres, numeros
+
+ruta = 'Evaluación 4/Ejercicios/pokemon.csv'
+nombres, numeros = crear_arboles(ruta)
