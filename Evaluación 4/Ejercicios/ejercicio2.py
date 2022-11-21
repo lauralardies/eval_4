cb0466db6e4bc4aeff3b5642cfd3f0d413788842
lauralardies <lrodrlar@myuax.com>
2022-11-21 18:37:08 +0100

    nse porque va mal. algo funciona

--- Evaluación 4/Ejercicios/ejercicio2.py
+++ Evaluación 4/Ejercicios/ejercicio2.py
@@ -48,31 +48,41 @@ def crear_arboles(ruta):
                 tipos = arbol_nombre(tipos, i[1], i[0], i[2])
     return nombres, numeros, tipos
 
-def busqueda_num(ruta, numero):
+def print_nodo(ruta, nodo):
     with open(ruta, 'r') as archivo:
         lector = reader(archivo)
         for i in lector:
-            if i[0] == numero:
-                return i
-        return 'No existe este número de Pokemon'
+            if i[0] == nodo.num:
+                return 'Pokémon número {}, {} de tipo {} y {} con\nTotal: {}\nHP: {}\nAttack: {}\nDefense: {}\nSp. Atk: {}\nSp. Def: {}\nSpeed: {}\nGeneration: {}\nLegendary: {}'.format(i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9], i[10], i[11], i[12])
 
-def busqueda_nom(ruta, nombre):
-    with open(ruta, 'r') as archivo:
-        lector = reader(archivo)
-        sol = []
-        for i in lector:
-            if i[1].startswith(nombre):
-                sol.append(i)
-        if len(sol) > 0:
-            return sol
-        else:
-            return 'No existen Pokemons con este nombre'
+def buscar_num(raiz, clave):
+    pos = None
+    if raiz is not None:
+        if raiz.num == clave: # Nos salimos cuando encontramos en nod que buscamos
+            pos = raiz
+            return pos
+        if pos == None:
+            pos = buscar_num(raiz.izq,clave)
+        if pos == None:
+            pos = buscar_num(raiz.der,clave)
+    return pos
+
+def buscar_nom(raiz, clave, pos = []):
+    if raiz is not None:
+        if raiz.nombre.startswith(clave):
+            pos.append(raiz)
+        pos = (buscar_nom(raiz.izq, clave, pos))
+        pos = (buscar_nom(raiz.der, clave, pos))
+    return pos
 
 ruta = 'Evaluación 4/Ejercicios/pokemon.csv'
 nombres, numeros, tipos = crear_arboles(ruta)
 
-print(busqueda_num(ruta, '6'))
-print(busqueda_num(ruta, '-9'))
+s = buscar_num(numeros, '6')
+g = buscar_num(numeros, '-9')
 
-print(busqueda_nom(ruta, 'Bee'))
-print(busqueda_nom(ruta, 'Zorz'))
+l = buscar_nom(nombres, 'Zorz')
+r = buscar_nom(nombres, 'Bee')
+
+for i in r:
+    print(print_nodo(ruta, i))
