24a27e88043941c5d28610571e85304a7ee45ca8
lauralardies <lrodrlar@myuax.com>
2022-11-21 17:49:11 +0100

    eliminamos variable padre

--- Evaluación 4/Ejercicios/ejercicio2.py
+++ Evaluación 4/Ejercicios/ejercicio2.py
@@ -4,7 +4,6 @@ class NodoArbol:
     def __init__(self, nombre, num, tipo):
         self.izq = None
         self.der = None
-        self.padre = None
         self.nombre = nombre
         self.num = num
         self.tipo = tipo
