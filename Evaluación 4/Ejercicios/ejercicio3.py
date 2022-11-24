18e3bfc0bdf1a57605ad1a348c2dcd7b3bf27210
lauralardies <lrodrlar@myuax.com>
2022-11-24 19:32:06 +0100

    clases

---
+++ Evaluación 4/Ejercicios/ejercicio3.py
@@ -0,0 +1,23 @@
+class NodoArista():
+    def __init__(self, info, destino):
+        self.info = info
+        self.destino = destino
+        self.sig = None
+
+class NodoVertice():
+    def __init__(self, info):
+        self.info = info
+        self.sig = None
+        self.visitado = False
+        self.adyacentes = Arista()
+
+class Grafo():
+    def __init__(self, dirigido=False):
+        self.inicio = None
+        self.dirigido = dirigido
+        self.tam = 0
+
+class Arista():
+    def __init__(self):
+        self.inicio = None
+        self.tam = 0
